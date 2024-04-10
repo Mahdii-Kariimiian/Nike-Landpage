72cf67c1e32f8da5000dfc682d9fa3e4361b51bc
Mahdi Karimian <mahdii.kariimiian@gmail.com>
2024-04-10 17:42:05 +0200

    add screenshots

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -5,7 +5,7 @@ function ServiceCard({ imgURL, label, subtext }) {
         <div className="flex-1 sm:w-[350px] sm:min-w-[350px] shadow-3xl rounded-[20px] py-16 px-10">
             <div className="w-11 h-11 flex justify-center items-center bg-coral-red rounded-full">
                 <img src={imgURL} alt={label} width={24} height={24} />
-            </div>
+            </div>  
             <h3 className="mt-5 font-palanquin text-3xl leading-normal font-bold">
                 {label}
             </h3>
